68aafdbf7fc8011f951d36b13429c1088f69bd25
Paulo Alves <107944987+Paulo110998@users.noreply.github.com>
2023-02-26 17:16:35 -0300

    Add files via upload

---
+++ paginas/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import Homepaginas, Sobre
+
+urlpatterns =[
+    path('', Homepaginas.as_view(), name="homepaginas"),
+    path('sobre', Sobre.as_view(), name="sobre"),
+
+]
