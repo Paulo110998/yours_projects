c9305359d5c478ff4891eb72086c593e3b681ad6
Paulo Alves <evertonpaulo1998@gmail.com>
2023-04-08 12:18:44 -0300

    agora vai?

--- yoursprojects/settings.py
+++ yoursprojects/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 from telnetlib import LOGOUT
 import os
-import django_on_heroku
+from django import django_on_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
