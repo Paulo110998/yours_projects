dd9edfe456a67cb2eedba2200d6860fd5f014cc4
Paulo Alves <evertonpaulo1998@gmail.com>
2023-04-17 15:19:05 -0300

    Projeto Atualizado

--- yoursprojects/settings.py
+++ yoursprojects/settings.py
@@ -13,7 +13,12 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 from telnetlib import LOGOUT
 import os
+import dj_database_url
 
+# Usando PostgreeSQL com Heroku
+DATABASES = {
+    'default': dj_database_url.config()
+}
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +31,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^!apz-vgmb8b64t=7f=c2u3omo9o6(kbhcd%nb#53ene(wchm0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True #Serve para as mensagens de erros apareçam em fase de desenvolvimento, ao fazer deploy, temos que retirar.
+DEBUG = False #Serve para as mensagens de erros apareçam em fase de desenvolvimento, ao fazer deploy, temos que retirar.
 
 ALLOWED_HOSTS = [] # Ao fazer deploy, temos que especificar o domínio da aplicação dentro do ALLOWED_HOSTS
 
@@ -53,7 +58,7 @@ INSTALLED_APPS = [
 # MIDDLEWARE = Mediador entre o cliente e o servidor - EX: Browser(cliente) -> Middleware <- Servidor
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    #"whitenoise.middleware.WhiteNoiseMiddleware", # White Noise -> Serve para arquivos estáticos
+    "whitenoise.middleware.WhiteNoiseMiddleware", # White Noise -> Serve para arquivos estáticos
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -87,7 +92,7 @@ WSGI_APPLICATION = 'yoursprojects.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -99,7 +104,7 @@ DATABASES = {
 
     }
 }
-
+"""
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -178,8 +183,9 @@ LOGOUT_REDIRECT = "login"
 # S M T P -> Simple Mail Transfer Protocol (Protocolo de envio de e-mail simples).
 #Mime -> É uma norma de envio de mensagens pela internet, padrão de envio de mensagem códificado.
 
-import smtplib
 
+"""
+import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText 
 
@@ -194,3 +200,4 @@ EMAIL_HOST_PASSWORD = "yhxkbvfujatnenaz"
 
 server = smtplib.SMTP(EMAIL_HOST, EMAIL_PORT)
 
+"""
